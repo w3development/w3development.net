d93c1942a0fda84dca6ea43e55d36fc9435856a0
Elvis Tavasja <tavasja@gmail.com>
2023-06-21 21:08:11 +0200

    Refactored typoscript configurations.

--- typo3conf/ext/theme/Configuration/TCA/Overrides/sys_template.php
+++ typo3conf/ext/theme/Configuration/TCA/Overrides/sys_template.php
@@ -7,6 +7,6 @@ defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'theme',
-    'Configuration/TypoScript/Main',
+    'Configuration/TypoScript',
     'Theme'
 );

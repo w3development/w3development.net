fb595cd322782f50b90059869cd76777e1f434d5
Elvis Tavasja <tavasja@gmail.com>
2023-06-21 18:24:16 +0200

    Removed photoswipe extension

--- typo3conf/ext/photoswipe/ext_tables.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die ('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('photoswipe', 'Configuration/TypoScript', 'PhotoSwipe');
--- typo3conf/ext/photoswipe/ext_tables.sql
+++
@@ -1,6 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	imagelazy64 tinyint(4) DEFAULT '0' NOT NULL
-);

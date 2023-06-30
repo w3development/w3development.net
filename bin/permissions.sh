0d9f35d8659d0a9eabf1681c28966d295fea5dfd
Elvis Tavasja <tavasja@gmail.com>
2023-06-30 18:29:07 +0200

    Refactored the script

--- bin/permissions.sh
+++ bin/permissions.sh
@@ -10,29 +10,4 @@ if [ -f "$LIBRARY" ]; then
 fi
 
 # Main function where the program starts
-
-## BEGIN
-run_if_root
-
-set_terminal_colors
-
-echo_inline "[1] Changing user group (www-data:www-data) ... "
-chown www-data.www-data -R .
-cmd_end
-
-sleep 1
-echo_inline "[2] Setting directory rights to 770 ... "
-find . -type d -exec chmod 2770 {} \;
-cmd_end
-
-sleep 1
-echo_inline "[3] Setting file rights to 660 ... "
-find . -type f -exec chmod 0660 {} \;
-cmd_end
-
-sleep 1
-echo_inline "[4] Making bin files executable ... "
-find bin/ -type f -exec chmod 0770 {} \;
-cmd_end
-
-## END
+fix_typo3_permissions

b97b645488f26501859d3f9192983ea9f29a885b
Elvis Tavasja <tavasja@gmail.com>
2023-07-02 03:37:32 +0200

    Removed powermailrecaptcha extension

--- typo3conf/ext/powermailrecaptcha/ext_tables.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Add Page TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:powermailrecaptcha/Configuration/PageTsConfig/Recaptcha.typoscript">'
-);

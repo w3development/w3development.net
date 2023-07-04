b4b7e8a8fc49558c1fc024f8a37422ee18037ed7
Elvis Tavasja <tavasja@gmail.com>
2023-07-04 19:13:24 +0200

    Added powermail extension

---
+++ html/typo3conf/ext/powermail/ext_emconf.php
@@ -0,0 +1,42 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "powermail".
+ *
+ * Auto generated 04-07-2023 15:25
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'powermail',
+  'description' => 'Powermail is a well-known, editor-friendly, powerful
+        and easy to use mailform extension with a lots of features
+        (spam prevention, marketing information, optin, ajax submit, diagram analysis, etc...)',
+  'category' => 'plugin',
+  'version' => '8.2.4',
+  'state' => 'stable',
+  'author' => 'Powermail Development Team',
+  'author_email' => 'alexander.kellner@in2code.de',
+  'author_company' => 'in2code.de',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '10.0.0-10.99.99',
+      'php' => '7.0.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+);
+

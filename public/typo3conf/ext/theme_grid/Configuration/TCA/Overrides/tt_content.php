8bbd6ff4fa6da8bdaaeb12119cbcbd0fbd41532e
Elvis Tavasja <tavasja@gmail.com>
2023-07-11 20:15:15 +0200

    Add container column configuration

--- public/typo3conf/ext/theme_grid/Configuration/TCA/Overrides/tt_content.php
+++ public/typo3conf/ext/theme_grid/Configuration/TCA/Overrides/tt_content.php
@@ -37,4 +37,83 @@
     ->setIcon('EXT:container/Resources/Public/Icons/container-2col.svg')
     ->setSaveAndCloseInNewContentElementWizard(false)
 );
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'container-2col-66-33', // CType
+            'Container - 2 columns (66-33)', // label
+            'Container - 2 Column (66-33)', // description
+            [
+                [
+                    [ 'name' => 'Leftside content' , 'colPos' => 211 ],
+                    [ 'name' => 'Rightside content' , 'colPos' => 212 ],
+                ]
+            ] // grid configuration
+        )
+    )
+    // set an optional icon configuration
+    // override default configurations
+    ->setIcon('EXT:container/Resources/Public/Icons/container-2col-left.svg')
+    ->setSaveAndCloseInNewContentElementWizard(false)
+);
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'container-2col-33-66', // CType
+            'Container - 2 columns (33-66)', // label
+            'Container - 2 Column (33-66)', // description
+            [
+                [
+                    [ 'name' => 'Leftside content' , 'colPos' => 221 ],
+                    [ 'name' => 'Rightside content' , 'colPos' => 222 ],
+                ]
+            ] // grid configuration
+        )
+    )
+    // set an optional icon configuration
+    // override default configurations
+    ->setIcon('EXT:container/Resources/Public/Icons/container-2col-right.svg')
+    ->setSaveAndCloseInNewContentElementWizard(false)
+);
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'container-2col-75-25', // CType
+            'Container - 2 columns (75-25)', // label
+            'Container - 2 Column (75-25)', // description
+            [
+                [
+                    [ 'name' => 'Leftside content' , 'colPos' => 231 ],
+                    [ 'name' => 'Rightside content' , 'colPos' => 232 ],
+                ]
+            ] // grid configuration
+        )
+    )
+    // set an optional icon configuration
+    // override default configurations
+    ->setIcon('EXT:container/Resources/Public/Icons/container-2col-left.svg')
+    ->setSaveAndCloseInNewContentElementWizard(false)
+);
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)->configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'container-2col-25-75', // CType
+            'Container - 2 columns (25-75)', // label
+            'Container - 2 Column (25-75)', // description
+            [
+                [
+                    [ 'name' => 'Leftside content' , 'colPos' => 241 ],
+                    [ 'name' => 'Rightside content' , 'colPos' => 242 ],
+                ]
+            ] // grid configuration
+        )
+    )
+    // set an optional icon configuration
+    // override default configurations
+    ->setIcon('EXT:container/Resources/Public/Icons/container-2col-right.svg')
+    ->setSaveAndCloseInNewContentElementWizard(false)
+);
 ?>

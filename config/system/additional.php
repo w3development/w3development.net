425f8ea7489bb19476fbc86a28d55f3d34d5fd65
Elvis Tavasja <tavasja@gmail.com>
2023-09-29 00:37:34 +0000

    Add additional settings.

--- config/system/additional.php
+++ config/system/additional.php
@@ -19,17 +19,24 @@ $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
             'processor' => 'ImageMagick',
             'processor_path' => '/usr/bin/',
             'processor_path_lzw' => '/usr/bin/',
+            'processor_colorspace' => 'sRGB',
+            'processor_effects' => true,
         ],
         // This mail configuration sends all emails to mailhog
         'MAIL' => [
             'transport' => 'smtp',
             'transport_smtp_encrypt' => false,
             'transport_smtp_server' => 'localhost:1025',
+            'transport_sendmail_command' => '/usr/sbin/sendmail -t -i -f webmaster@dev.w3development.net',
+            'defaultMailFromAddress' => 'no-reply@w3development.net'
         ],
         'SYS' => [
             'trustedHostsPattern' => '.*.*',
             'devIPmask' => '*',
             'displayErrors' => 1,
         ],
+        'BE' => [
+            'lockSSL' => 1
+        ]
     ]
 );

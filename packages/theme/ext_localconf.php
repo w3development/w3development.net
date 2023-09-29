d0c24b72a887f153930a84015d6ddbce18dbd09e
Elvis Tavasja <tavasja@gmail.com>
2023-09-29 17:06:53 +0000

    TCA Configurations update

--- packages/theme/ext_localconf.php
+++ packages/theme/ext_localconf.php
@@ -5,35 +5,6 @@ if (!defined('TYPO3')) die('Access denied.');
 // $currentApplicationContext = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext();
 // $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] .= ' (' . (string)$currentApplicationContext . ')';
 
-/**
- * Adding the default backend layout TSconfig
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme/Configuration/TSconfig/Page/Mod/WebLayout/BackendLayouts.tsconfig">
-');
-
-/**
- * Adding the default page TSconfig
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme/Configuration/TSconfig/Page/TCEFORM.tsconfig">
-');
-
-
-/**
- * Adding the SHARED.TSconfig
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme/Configuration/TSconfig/Page/Mod/SHARED.tsconfig">
-');
-
-/**
- * Adding the default user TSconfig
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme/Configuration/TSconfig/User/options.tsconfig">'
-);
-
 // call_user_func(function () {
 //     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
 //         trim('

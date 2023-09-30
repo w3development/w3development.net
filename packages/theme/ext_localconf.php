c91797e3b189c4ca620b79b195a10a4be167185c
Elvis Tavasja <tavasja@gmail.com>
2023-09-30 17:12:11 +0000

    Load typoscript automatically

--- packages/theme/ext_localconf.php
+++ packages/theme/ext_localconf.php
@@ -6,9 +6,21 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
+define(THEME, 'theme');
+
 /**
  * Adding the default user TSconfig
  */
 ExtensionManagementUtility::addUserTSConfig(
-    '@import "EXT:theme/Configuration/TsConfig/User/options.tsconfig"'
+    '@import "EXT:'. THEME . '/Configuration/TsConfig/User/options.tsconfig"'
 );
+
+
+call_user_func(function()
+{
+    ExtensionManagementUtility::addTypoScript(
+      THEME,
+      'setup',
+      "@import 'EXT:$extensionKey/Configuration/TypoScript/setup.typoscript'"
+   );
+});
